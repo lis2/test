fb65c3092d53299e06f582f9c6bfba58a1a64f1a
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2025-04-02 13:43:23 +0800

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-<a href="google.com">google</a>
+<a href="https://google.com">google</a>
 <img src="https://media1.tenor.com/m/yheo1GGu3FwAAAAC/rick-roll-rick-ashley.gif" />
 <script type="text/javascript">
   alert("fsdfsd");
